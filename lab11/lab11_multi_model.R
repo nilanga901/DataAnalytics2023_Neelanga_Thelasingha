62e56517eed4663b9314c1f389f6fca66a3b5e0c
Neelanga chandrajith thelasingha <43623371+nilanga901@users.noreply.github.com>
2023-12-08 21:38:33 -0500

    lab11

---
+++ lab11/lab11_multi_model.R
@@ -0,0 +1,86 @@
+install.packages("caret")
+install.packages("caret", dependencies=c("Depends", "Suggests"))
+library(caret)
+# attach the iris dataset to the environment
+data(iris)
+# rename the dataset
+dataset <- iris
+
+validation_index <- createDataPartition(dataset$Species, p=0.80, list=FALSE)
+# select 20% of the data for validation
+validation <- dataset[-validation_index,]
+# use the remaining 80% of data to training and testing the models
+dataset <- dataset[validation_index,]
+# dimensions of dataset
+dim(dataset)
+
+sapply(dataset, class)
+# take a peek at the first 6 rows of the data
+head(dataset)
+# list the levels for the class
+levels(dataset$Species)
+# summarize the class distribution
+percentage <- prop.table(table(dataset$Species)) * 100
+cbind(freq=table(dataset$Species), percentage=percentage)
+
+
+# summarize attribute distributions
+summary(dataset)
+# plots
+# split input and output
+x <- dataset[,1:4]
+y <- dataset[,5]
+# boxplot for each attribute on one image
+par(mfrow=c(1,4))
+for(i in 1:4) {
+  boxplot(x[,i], main=names(iris)[i])
+}
+
+
+# barplot for class breakdown
+plot(y)
+# Multivariate Plots
+# scatterplot matrix
+featurePlot(x=x, y=y, plot="ellipse")
+# box and whisker plots for each attribute
+featurePlot(x=x, y=y, plot="box")
+# density plots for each attribute by class value
+scales <- list(x=list(relation="free"), y=list(relation="free"))
+featurePlot(x=x, y=y, plot="density", scales=scales)
+
+#Build Models
+# a) linear algorithms
+set.seed(7)
+fit.lda <- train(Species~., data=dataset, method="lda", metric=metric, trControl=control)
+# b) nonlinear algorithms
+# CART
+set.seed(7)
+fit.cart <- train(Species~., data=dataset, method="rpart", metric=metric, trControl=control)
+# kNN
+set.seed(7)
+fit.knn <- train(Species~., data=dataset, method="knn", metric=metric, trControl=control)
+# c) advanced algorithms
+# SVM
+set.seed(7)
+fit.svm <- train(Species~., data=dataset, method="svmRadial", metric=metric, trControl=control)
+# Random Forest
+set.seed(7)
+fit.rf <- train(Species~., data=dataset, method="rf", metric=metric, trControl=control)
+# summarize accuracy of models
+results <- resamples(list(lda=fit.lda, cart=fit.cart, knn=fit.knn, svm=fit.svm, rf=fit.rf))
+summary(results)
+# compare accuracy of models
+dotplot(results)
+# summarize Best Model
+print(fit.lda)
+# estimate skill of LDA on the validation dataset
+predictions <- predict(fit.lda, validation)
+confusionMatrix(predictions, validation$Species)
+
+
+
+
+
+
+
+
